bb05abcea045befb417443b248a6f2e8cf695254
liuhaibin123456789 <3398341353@qq.com>
2022-06-23 14:20:07 +0800

    day13

--- src/binaryTree/huffmanCode.java
+++ src/binaryTree/huffmanCode.java
@@ -1,45 +1,72 @@
 package binaryTree;
 
-import javax.swing.plaf.basic.BasicDesktopIconUI;
 import java.util.*;
 
 public class huffmanCode {
-    static Map<Character, String> CodeTable = new HashMap<>();//哈夫曼编码表
-    static StringBuilder Code=new StringBuilder();//暂存
+    static StringBuilder Code = new StringBuilder();//暂存
+    static Map<Byte, String> CodeTable = new HashMap<>();//哈夫曼编码表
 
     public static void main(String[] args) {
         String text = "i like like like java do you like a java";
         System.out.println("字符串长度：" + text.length());
         //转化为字符数组
-        char[] chs = text.toCharArray();
-        List<node1> nodes = getNodes(chs);
-
-        node1 root = createHuffmanTree(nodes);
-        System.out.println("前序遍历：");
-        root.preShow(root);
-
-        System.out.println("哈夫曼编码：");
-
-        StringBuilder result=new StringBuilder();
-        getHuffmanCode(root,"",result);
-        System.out.println("编码:"+Code);
-        System.out.println("编码表："+CodeTable.toString());
+        byte[] chs = text.getBytes();
+        System.out.println(Arrays.toString(huffmanZip(chs)));
     }
 
-    //得到所有叶子节点的哈夫曼编码
+    //将原始字符串的bytes数据编码并压缩，返回压缩结果
+    public static byte[] huffmanZip(byte[] bytes) {
+        //根据原始数据生成哈夫曼编码树
+        List<node1> nodes = getNodes(bytes);
+        node1 root = createHuffmanTree(nodes);
+        //根据生成的哈夫曼编码树生成编码表
+        StringBuilder result = new StringBuilder();
+        getHuffmanCode(root, "", result);
+
+        //根据哈夫曼编码表和原始bytes数组数据，进行压缩
+        return zip(bytes, CodeTable);
+    }
+
+    //根据赫夫曼编码表，将字符串的byte数组，返回一个赫夫曼编码，压缩后的byte数组
+    public static byte[] zip(byte[] oldBytes, Map<Byte, String> codeTable) {
+        StringBuilder builder = new StringBuilder();
+        //根据哈夫曼编码表，将byte数组转化为编码数据（变长了）
+        //这个编码数据还不是最后的压缩结果，根据显示，显然这个数据的长度还变长了，并没有压缩
+        for (byte b : oldBytes) {
+            builder.append(codeTable.get(b));
+        }
+        //将编码的字符串数据进行压缩，每8位一个字节数据,不足8位的往前补一位
+        int len;
+        int index = 0;
+        if (builder.length() % 8 == 0) len = builder.length() / 8;
+        else len = builder.length() / 8 + 1;
+        //创建压缩后的byte数组
+        byte[] compressedBytes = new byte[len];
+        for (int i = 0; i < builder.length(); i += 8) {
+            String strByte;
+            if (i + 8 > builder.length()) {
+                strByte = builder.substring(i);
+            } else {
+                strByte = builder.substring(i, i + 8);
+            }
+            compressedBytes[index++] = (byte) Integer.parseInt(strByte, 2);
+        }
+        return compressedBytes;
+    }
+
+    //得到所有叶子节点的哈夫曼编码表
     public static void getHuffmanCode(node1 node, String code, StringBuilder result) {
+
         StringBuilder code1 = new StringBuilder(result);
         code1.append(code);
         if (node != null) {
-            if (node.data == 0 ) {//非叶子节点
+            if (node.data == 0) {//非叶子节点
                 getHuffmanCode(node.left, "0", code1);//左子节点递归
                 getHuffmanCode(node.right, "1", code1);//右子节点递归
             } else {//叶子节点
-                Code.append(code1);
-                CodeTable.put(node.data,code1.toString());
+                CodeTable.put(node.data, code1.toString());
             }
         }
-//        return code1;
     }
 
     //创建哈夫曼树，返回父节点
@@ -63,10 +90,10 @@ public class huffmanCode {
     }
 
     //统计每个字符出现的次数（权重），然后记录在链表里
-    public static List<node1> getNodes(char[] chs) {
+    public static List<node1> getNodes(byte[] chs) {
         ArrayList<node1> nodes = new ArrayList<>();
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (char ch : chs) {
+        HashMap<Byte, Integer> map = new HashMap<>();
+        for (byte ch : chs) {
             Integer weight = map.get(ch);
             if (weight == null) {
                 map.put(ch, 1);
@@ -74,9 +101,9 @@ public class huffmanCode {
                 map.put(ch, weight + 1);
             }
         }
-        //将map里的值转化为node1并华为链表
+        //将map里的值转化为node1并化为链表
         //遍历map
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Byte, Integer> entry : map.entrySet()) {
             nodes.add(new node1(entry.getKey(), entry.getValue()));
         }
         return nodes;
@@ -84,7 +111,7 @@ public class huffmanCode {
 }
 
 class node1 implements Comparable<node1> {
-    char data;//存放的数据
+    byte data;//存放的数据
     int weight;//权值
     node1 left;
     node1 right;
@@ -109,7 +136,7 @@ class node1 implements Comparable<node1> {
         this.weight = weight;
     }
 
-    public node1(char data, int weight) {
+    public node1(byte data, int weight) {
         this.data = data;
         this.weight = weight;
     }
